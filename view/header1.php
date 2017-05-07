b8486485afd8c7420ba1bdba9c557a3d87f86845
sp2254 <sp2254@njit.edu>
2017-05-07 01:53:58 -0400

    seperated header for before login and after login and logout functionality

---
+++ view/header1.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<html>
+<head>
+<title>Todo appllication</title> 
+
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+  div.s1 {
+    position: absolute;
+    top: 150px;
+    left: 500px;
+    width: 200px;
+    height: 10px;
+    border: none;
+}
+div.head1 {
+    position: absolute;
+    top: 0px;
+    
+    width: 100%;
+    height: 13%;
+    background-color:grey;
+}
+h1   {color: #2e6da4;
+font-size:50px;
+
+}
+h2   {color: #2e6da4;
+font-size:20px;
+
+}
+</style>
+<script>
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+</script>
+
+</head> 
+
+<body>
+<div class="head1">
+<h1 align="center">Todo Application</h1>
+
+</div>
+
+
+
+<form action= "logout.php" method="post">
+ <a href = "logout.php">logout</a>
+ <input id="button"  type="submit" value="Logout" style="float: right;" data-toggle="tooltip" title="Logout"/>
+ </form>
+ 
+
+	</body>
+
+
+
+ 
+ </html>
